1c31ffd90830f0da35dceefdb6b4739bd3f8fd44
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-11-16 13:52:33 -0800

    Created a headers file.

---
+++ fork_child.c
@@ -0,0 +1,35 @@
+#include "main.h"
+ 
+/**
+ * fork_child - This creates a child process and executes a program.
+ * @arg: Array of string arguments.
+ * @argv: Argument to main.
+ *
+ * Return: 0 on success, otherwise -1.
+ */
+
+int fork_child(char **argv, char **arg)
+{
+	pid_t pid; /* child process */
+	int status;
+
+	if (access(arg[0], F_OK) == -1)
+	{
+		perror(argv[0]);
+		return (0);
+	}
+	pid = fork();
+	if (pid < 0) /* error */
+		return (-1);
+
+	if (pid == 0) /* child process */
+	{
+		if (execve(arg[0], arg, environ) == -1)
+			perror(argv[0]);
+	}
+
+	else if (pid > 0) /* parent process */
+		wait(&status);
+
+	return (0);
+}
--- main.h
+++ main.h
@@ -11,11 +11,13 @@ void prompt(char *s);
 extern char **environ;
 int _putchar(char c);
 void copy_string(char **arg, char *line_cpy);
-void free_memory(char *mem1, char *mem2, char **mem3);
-char *_strcpy(char *dest, char *src, int bytes);
+
+char *_strcpy(char *dest, char *src);
 ssize_t read_command(char *line_ptr, size_t n);
 void rem_newline(char *line);
 int fork_child(char **argv, char **arg);
 int get_token(char **arg, char *line_ptr);
+
+int leave_prog(char *line_ptr);
 
 #endif /* #define MAIN_H */
