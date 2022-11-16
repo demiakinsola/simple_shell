427ee481d56b99fa3dc185749ef90853a743489d
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-11-16 13:56:45 -0800

    Created a function that prints characters.

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - This prits a character.
+ * @c: Character to print.
+ * Return: 0 on success, else -1.
+ */
+
+int _putchar(char c)
+{
+	write(1, &c, 1);
+		return (0);
+}
